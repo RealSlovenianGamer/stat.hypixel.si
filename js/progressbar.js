41b2a8784bf43abaebbebf184afb428d07fe1e6f
Marko <marko.a.medvesek@gmail.com>
2022-08-14 12:10:45 +0200

    beta v0.1.0
    Changelog:
    - Na main page dodal background, Circular progress bar za št članov
    - logoti, navbar, stili, itd. itd.

---
+++ js/progressbar.js
@@ -0,0 +1,21 @@
+let circularProgress = document.querySelector(".circular-progress"),
+progressValue = document.querySelector(".progress-value");
+
+fetch('http://localhost:8080/guild')
+.then(response => response.json())
+.then(data => {
+    //progressEndValue = Math.floor(100 / data.guild.members.length);
+    progressEndValue = Math.floor(100 / data.guild.members.length * 100);
+    var allmembers = data.guild.members.length;
+    let progressStartValue = 0,    
+    speed = 40;
+    let progress = setInterval(() => {
+    progressStartValue++;
+    progressValue.textContent = `${allmembers} članov`
+    circularProgress.style.background = `conic-gradient(#7d2ae8 ${progressStartValue * 3.6}deg, #ededed 0deg)`
+
+    if(progressStartValue == progressEndValue){
+        clearInterval(progress);
+    }    
+    }, speed);
+})
